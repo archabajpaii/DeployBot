616a0553cb36904e1047ea66354153dfd6cee30d
Archa Bajpai <archabajpai4@gmail.com>
2025-07-06 12:45:02 +0530

    new commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,30 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_security_group" "deploybot_sg" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "deploybot_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.deploybot_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
